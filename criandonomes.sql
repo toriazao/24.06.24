b69acab201cd9dc6ed865073c5f083c04fdc0a58
toriazao <142436669+toriazao@users.noreply.github.com>
2024-06-24 12:27:02 -0300

    Add files via upload

---
+++ criandonomes.sql
@@ -0,0 +1,6 @@
+insert into `tbl_pessoas` (`nome`) values ('Alfredo');
+insert into `tbl_pessoas` (`nome`) values ('Bento');
+insert into `tbl_pessoas` (`nome`) values ('Charles');
+insert into `tbl_pessoas` (`nome`) values ('Dinelson');
+insert into `tbl_pessoas` (`nome`) values ('Eufrates');
+insert into `tbl_pessoas` (`nome`) values ('Franklin');
